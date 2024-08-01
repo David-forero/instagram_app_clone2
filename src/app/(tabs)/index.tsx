0a94290088c795a3408a7639e9fd4782b9d79a4c
David Forero <david.forero1813@gmail.com>
2024-07-31 21:41:19 -0400

    feed screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Text, View, FlatList } from 'react-native';
+import PostListItem from '~/src/components/PostListItem';
+import posts from '~/assets/data/posts.json';
+
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app ssss!</Text>
-      <StatusBar style="auto" />
+    <View className='bg-white'>
+      <FlatList
+        data={posts}
+        renderItem={({item}) => <PostListItem post={item} />}
+        contentContainerStyle={{gap: 10}}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
